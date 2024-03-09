5adcedab1caf67980ff9a9565dcedec17f6db5e3
Britannio <britanniojarrett@gmail.com>
2024-03-09 21:00:51 +0000

    format cybergear.[c/h]

--- cybergear.h
+++ cybergear.h
@@ -26,28 +26,28 @@
 #define KD_MAX 5.0f
 #define T_MIN (-12.0f)
 #define T_MAX 12.0f
-//#define MAX_P 720
-//#define MIN_P -720
+// #define MAX_P 720
+// #define MIN_P -720
 #define MAX_P 12.5663706144f
 #define MIN_P (-12.5663706144f)
 
-#define Master_CAN_ID 0x00                      // Master CAN ID
-//Control commands
-#define Communication_Type_GetID 0x00           //Get motor ID
-#define Communication_Type_MotionControl 0x01 	//Send motion control command
-#define Communication_Type_MotorRequest 0x02	//Request motor status
-#define Communication_Type_MotorEnable 0x03	    //Enable the motor
-#define Communication_Type_MotorStop 0x04	    //Disable the motor
-#define Communication_Type_SetPosZero 0x06	    //Set the mechanical zero position
-#define Communication_Type_CanID 0x07	        //Change the CAN ID of the motor
+#define Master_CAN_ID 0x00 // Master CAN ID
+// Control commands
+#define Communication_Type_GetID 0x00         // Get motor ID
+#define Communication_Type_MotionControl 0x01 // Send motion control command
+#define Communication_Type_MotorRequest 0x02  // Request motor status
+#define Communication_Type_MotorEnable 0x03   // Enable the motor
+#define Communication_Type_MotorStop 0x04     // Disable the motor
+#define Communication_Type_SetPosZero 0x06    // Set the mechanical zero position
+#define Communication_Type_CanID 0x07         // Change the CAN ID of the motor
 #define Communication_Type_Control_Mode 0x12
-#define Communication_Type_GetSingleParameter 0x11	//Get Single Parameter
-#define Communication_Type_SetSingleParameter 0x12	//SetSingleParameter
-#define Communication_Type_ErrorFeedback 0x15	    //Error Feedback
+#define Communication_Type_GetSingleParameter 0x11 // Get Single Parameter
+#define Communication_Type_SetSingleParameter 0x12 // SetSingleParameter
+#define Communication_Type_ErrorFeedback 0x15      // Error Feedback
 // Parameter Read Macro Definition
 #define Run_mode 0x7005
-#define Iq_Ref   0x7006
-#define Spd_Ref  0x700A
+#define Iq_Ref 0x7006
+#define Spd_Ref 0x700A
 #define Limit_Torque 0x700B
 #define Cur_Kp 0x7010
 #define Cur_Ki 0x7011
@@ -56,13 +56,13 @@
 #define Limit_Spd 0x7017
 #define Limit_Cur 0x7018
 
-#define Gain_Angle 720/32767.0
+#define Gain_Angle 720 / 32767.0
 #define Bias_Angle 0x8000
-#define Gain_Speed 30/32767.0
+#define Gain_Speed 30 / 32767.0
 #define Bias_Speed 0x8000
-#define Gain_Torque 12/32767.0
+#define Gain_Torque 12 / 32767.0
 #define Bias_Torque 0x8000
-#define Temp_Gain   0.1
+#define Temp_Gain 0.1
 
 #define Motor_Error 0x00
 #define Motor_OK 0X01
@@ -70,32 +70,33 @@
 #define MOTOR0_ANGLE_OFFSET 0.558952f
 #define MOTOR1_ANGLE_OFFSET (-0.716955f)
 
-enum CONTROL_MODE   //Control modes
+enum CONTROL_MODE // Control modes
 {
-    Motion_mode = 0,//Motion control
-    Position_mode,  //Position control
-    Speed_mode,     //Speed control
-    Current_mode    //Torque control
+    Motion_mode = 0, // Motion control
+    Position_mode,   // Position control
+    Speed_mode,      // Speed control
+    Current_mode     // Torque control
 };
 
-enum ERROR_TAG      //Error Tags 
+enum ERROR_TAG // Error Tags
 {
-    OK                 = 0,//No error
-    BAT_LOW_ERR        = 1,//Low battery voltage
-    OVER_CURRENT_ERR   = 2,//Overcurrent
-    OVER_TEMP_ERR      = 3,//Overheating
-    MAGNETIC_ERR       = 4,//Magnetic sensor fail
-    HALL_ERR_ERR       = 5,//Hall encoder fail
-    NO_CALIBRATION_ERR = 6//Not calibrated
+    OK = 0,                // No error
+    BAT_LOW_ERR = 1,       // Low battery voltage
+    OVER_CURRENT_ERR = 2,  // Overcurrent
+    OVER_TEMP_ERR = 3,     // Overheating
+    MAGNETIC_ERR = 4,      // Magnetic sensor fail
+    HALL_ERR_ERR = 5,      // Hall encoder fail
+    NO_CALIBRATION_ERR = 6 // Not calibrated
 };
 
-typedef struct{           //Mi motor callback struct
-    uint8_t CAN_ID;       //CAN ID
-    uint8_t MCU_ID;       //MCU ID[last 8 bits only，64 bits in total]
-    float Angle;          //Motor angle
-    float Speed;          //Motor speed
-    float Torque;         //Motor torque
-    float Temp;			  //Motor temperature
+typedef struct
+{                   // Mi motor callback struct
+    uint8_t CAN_ID; // CAN ID
+    uint8_t MCU_ID; // MCU ID[last 8 bits only，64 bits in total]
+    float Angle;    // Motor angle
+    float Speed;    // Motor speed
+    float Torque;   // Motor torque
+    float Temp;     // Motor temperature
 
     uint16_t set_current;
     uint16_t set_speed;
@@ -106,7 +107,7 @@ typedef struct{           //Mi motor callback struct
     float Angle_Bias;
 
 } MI_Motor;
-extern MI_Motor mi_motor[2];//Define two motors here
+extern MI_Motor mi_motor[2]; // Define two motors here
 
 extern void check_cybergear(uint8_t ID);
 extern void start_cybergear(MI_Motor *Motor);
@@ -116,6 +117,6 @@ extern void set_current_cybergear(MI_Motor *Motor, float Current);
 extern void set_zeropos_cybergear(MI_Motor *Motor);
 extern void set_CANID_cybergear(MI_Motor *Motor, uint8_t CAN_ID);
 extern void init_cybergear(MI_Motor *Motor, uint8_t Can_Id, uint8_t mode);
-extern void motor_controlmode(MI_Motor *Motor,float torque, float MechPosition, float speed, float kp, float kd);
-extern void motor_position_control(MI_Motor * Motor, float desiredPosition);
-extern void motor_get_angle(MI_Motor * Motor);
+extern void motor_controlmode(MI_Motor *Motor, float torque, float MechPosition, float speed, float kp, float kd);
+extern void motor_position_control(MI_Motor *Motor, float desiredPosition);
+extern void motor_get_angle(MI_Motor *Motor);
