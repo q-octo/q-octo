0ac6074368884457e90dfb7f76148e2aab4c7cf7
Britannio <britanniojarrett@gmail.com>
2024-04-02 22:27:11 +0100

    arduino ws experiment

--- src_pico/src/main.cpp
+++ src_pico/src/main.cpp
@@ -3,15 +3,18 @@
 #include <FreeRTOS.h> // Enables FreeRTOS and multicore support
 #include <task.h>     // Enables FreeRTOS tasks
 
+#include <WiFi.h>
 #include "can.h"
 #include "task_control_motors.h"
 #include "task_crsf.h"
 #include "task_display.h"
 #include "task_motors.h"
+#include "web_server.h"
 
 #define ENABLE_CAN 0
-#define ENABLE_MOTORS 1
-#define ENABLE_DISPLAY 1
+#define ENABLE_MOTORS 0
+#define ENABLE_DISPLAY 0
+#define ENABLE_WEB_SERVER 0
 #define DEBUG_LIST_TASKS 0
 #define CORE_0 (1 << 0)
 #define CORE_1 (1 << 1)
@@ -41,7 +44,8 @@ std::map<eTaskState, const char *> eTaskStateName{{eReady, "Ready"},
                                                   {eSuspended, "Suspended"},
                                                   {eDeleted, "Deleted"}};
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   while (!Serial)
     ;          // Wait for serial connection to be established
@@ -60,7 +64,7 @@ void setup() {
 
 #if ENABLE_DISPLAY
   // Consumer (unless it is toggling diagnostics mode)
-  xTaskCreate(taskDisplay, "taskDisplay", configMINIMAL_STACK_SIZE * (2^1), nullptr, 3, &displayHandle);
+  xTaskCreate(taskDisplay, "taskDisplay", configMINIMAL_STACK_SIZE * (2 ^ 1), nullptr, 3, &displayHandle);
   vTaskCoreAffinitySet(displayHandle, CORE_1);
 #endif
 
@@ -85,25 +89,32 @@ void setup() {
   // Producer
   xTaskCreate(taskMotors, "taskMotors", configMINIMAL_STACK_SIZE, nullptr, 1, &motorsHandle);
   vTaskCoreAffinitySet(motorsHandle, CORE_1);
-#endif  
+#endif
 }
 
 // Handled by FreeRTOS
-void loop() {
+void loop()
+{
 #if DEBUG_LIST_TASKS
   printTaskStatus();
   delay(5000);
 #endif
+#if ENABLE_WEB_SERVER
+  WSWebServer::start();
+  WSWebServer::loop();
+#endif
 }
 
-void printTaskStatus() {
+void printTaskStatus()
+{
   uint32_t tasks = uxTaskGetNumberOfTasks();
   auto *pxTaskStatusArray = new TaskStatus_t[tasks];
   unsigned long runtime;
   tasks = uxTaskGetSystemState(pxTaskStatusArray, tasks, &runtime);
   Serial.printf("# Tasks: %lu\n", tasks);
   Serial.println("ID, NAME, STATE, PRIO, CYCLES");
-  for (int i = 0; i < tasks; i++) {
+  for (int i = 0; i < tasks; i++)
+  {
     Serial.printf("%d: %-16s %-10s %d %lu\n", i,
                   pxTaskStatusArray[i].pcTaskName,
                   eTaskStateName[pxTaskStatusArray[i].eCurrentState],
@@ -113,30 +124,36 @@ void printTaskStatus() {
   delete[] pxTaskStatusArray;
 }
 
-void taskWatchdog(void *pvParameters) {
+void taskWatchdog(void *pvParameters)
+{
   (void)pvParameters; //  To avoid warnings
   Serial.println("taskWatchdog started");
-  for (;;) {
+  for (;;)
+  {
     Serial.println("taskWatchdog tick");
     delay(pdMS_TO_TICKS(2000));
   }
 }
 
-void taskCAN(void *pvParameters) {
+void taskCAN(void *pvParameters)
+{
   (void)pvParameters; //  To avoid warnings
   Serial.println("taskCAN started");
-  for (;;) {
+  for (;;)
+  {
     CanCommunication::checkForPacket();
     delay(pdMS_TO_TICKS(1));
   }
 }
 
 void onReceiveCanPacket(int packetSize, uint32_t packetId, uint8_t *packetData,
-                        bool extended) {
+                        bool extended)
+{
   Serial.print("main.cpp: Received packet with id 0x");
   Serial.print(packetId, HEX);
 
-  switch ((packetId & 0xFF00) >> 8) {
+  switch ((packetId & 0xFF00) >> 8)
+  {
   case CYBERGEAR_CAN_ID_L:
     cybergearL.process_message(packetData);
     break;
---
+++ src_pico/src/web_server.cpp
@@ -0,0 +1,149 @@
+
+#include <Arduino.h>
+#include "web_server.h"
+#include <WebSocketsServer.h>
+
+/*
+The web server is off by default.
+When instructed, it will be started.
+It can be stopped.
+For now, we'll assume that one device will be connected at any time?? hmm.
+Anyway, it will establish a web socket connection and from there we can send
+commands back and fourth as we wish.
+Think back to the distributed systems & networks coursework, it's like this.
+Create a list of instructions and the shape of their associated payload.
+
+First action will likely be the server sending all current configuration info.
+I imagine that a key-value system will work for all the properties to be
+managed, this way we can add new configurations with minimal changes everywhere.
+
+
+*/
+
+#define USE_SERIAL Serial
+
+int status = WL_IDLE_STATUS; // the WiFi server status
+WiFiServer wifiServer(80);
+WebSocketsServer webSocket = WebSocketsServer(81);
+WiFiMulti wifi = WiFiMulti();
+
+bool webServerIsRunning = false;
+bool webServerInitialised = false;
+
+void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t length);
+
+void initWebServer()
+{
+    if (webServerInitialised)
+    {
+        return;
+    }
+    wifi.addAP("paganello", "20242024");
+    webServerInitialised = true;
+}
+
+void WSWebServer::start()
+{
+    if (webServerIsRunning)
+    {
+        return;
+    }
+    USE_SERIAL.println("Starting web server");
+    if (!webServerInitialised)
+    {
+        initWebServer();
+    }
+    while (wifi.run() != WL_CONNECTED)
+    {
+        delay(100);
+    }
+    webSocket.begin();
+    webSocket.onEvent(webSocketEvent);
+
+    // handle index
+    // wifiServer.on("/", []()
+    //               {
+    //     // send index.html
+    //     wifiServer.send(200, "text/html", "<html><head><script>var connection = new WebSocket('ws://'+location.hostname+':81/', ['arduino']);connection.onopen = function () {  connection.send('Connect ' + new Date()); }; connection.onerror = function (error) {    console.log('WebSocket Error ', error);};connection.onmessage = function (e) {  console.log('Server: ', e.data);};function sendRGB() {  var r = parseInt(document.getElementById('r').value).toString(16);  var g = parseInt(document.getElementById('g').value).toString(16);  var b = parseInt(document.getElementById('b').value).toString(16);  if(r.length < 2) { r = '0' + r; }   if(g.length < 2) { g = '0' + g; }   if(b.length < 2) { b = '0' + b; }   var rgb = '#'+r+g+b;    console.log('RGB: ' + rgb); connection.send(rgb); }</script></head><body>LED Control:<br/><br/>R: <input id=\"r\" type=\"range\" min=\"0\" max=\"255\" step=\"1\" oninput=\"sendRGB();\" /><br/>G: <input id=\"g\" type=\"range\" min=\"0\" max=\"255\" step=\"1\" oninput=\"sendRGB();\" /><br/>B: <input id=\"b\" type=\"range\" min=\"0\" max=\"255\" step=\"1\" oninput=\"sendRGB();\" /><br/></body></html>"); });
+
+    wifiServer.begin();
+
+    webServerIsRunning = true;
+}
+
+void WSWebServer::stop()
+{
+    if (!webServerIsRunning)
+    {
+        return;
+    }
+    webSocket.close();
+    webServerIsRunning = false;
+}
+
+void WSWebServer::loop()
+{
+    webSocket.loop();
+    WiFiClient client = wifiServer.accept();
+    if (client)
+    {
+        while (client.connected())
+        {
+            if (client.available())
+            {
+                USE_SERIAL.println("Connected to /");
+                char c = client.read();
+                // Process the request and send a response
+                client.println("HTTP/1.1 200 OK");
+                client.println("Content-Type: text/html");
+                client.println("Connection: close");
+                client.println();
+                client.println("<!DOCTYPE HTML>");
+                client.println("<html>");
+                // Add your HTML content here
+                client.println("Something works :)");
+                client.println("</html>");
+                break;
+            }
+        }
+        // Can probably remove the delay
+        delay(1);
+        client.stop();
+    }
+}
+
+void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t length)
+{
+
+    switch (type)
+    {
+    case WStype_DISCONNECTED:
+        USE_SERIAL.printf("[%u] Disconnected!\n", num);
+        break;
+    case WStype_CONNECTED:
+    {
+        IPAddress ip = webSocket.remoteIP(num);
+        USE_SERIAL.printf("[%u] Connected from %d.%d.%d.%d url: %s\n", num, ip[0], ip[1], ip[2], ip[3], payload);
+
+        // send message to client
+        webSocket.sendTXT(num, "Connected");
+    }
+    break;
+    case WStype_TEXT:
+        USE_SERIAL.printf("[%u] get Text: %s\n", num, payload);
+
+        // send message to client
+        // webSocket.sendTXT(num, "message here");
+
+        // send data to all connected clients
+        // webSocket.broadcastTXT("message here");
+        break;
+    case WStype_BIN:
+        USE_SERIAL.printf("[%u] get binary length: %u\n", num, length);
+        hexdump(payload, length);
+
+        // send message to client
+        // webSocket.sendBIN(num, payload, length);
+        break;
+    }
+}
