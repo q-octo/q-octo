c6f6a38a2e5e1743f9cd9dfd1cbbdc36005ed49b
Niall Morrison <champion.pillip@gmail.com>
2024-04-08 23:15:30 +0100

    FreeRTOS Listening to UART

--- src_companion/main.c
+++ src_companion/main.c
@@ -1,64 +1,73 @@
 // Copyright (c) 2020-2022 Cesanta Software Limited
 // All rights reserved
 
-#include "pico/cyw43_arch.h"
+//#include "pico/cyw43_arch.h"
+//#include "dhcpserver.h"
 #include "pico/stdlib.h"
-
+#include <stdio.h>
 #include "lwip/ip4_addr.h"
-#include "dhcpserver.h"
 
 #include "FreeRTOS.h"
 #include "task.h"
 #include "mongoose.h"
 #include "net.h"
 
-#include "blink.h"
+#include "hardware/uart.h"
 
 
 #define TEST_TASK_PRIORITY				( tskIDLE_PRIORITY + 1UL )
 #define TEST_TASK_STACK_SIZE			(( configSTACK_DEPTH_TYPE ) 2048)
 
+// UART configuration
+#define UART_ID uart0
+#define BAUD_RATE 115200
+#define UART_TX_PIN 0
+#define UART_RX_PIN 1
+#define MAX_STRING_SIZE 100 // Adjust based on your needs
+
+
 static struct mg_mgr mgr;
 
-void main_task(__unused void *params) {
-    if (cyw43_arch_init()) {
-        printf("failed to initialise\n");
-        return;
+void uart_receive_task(void *pvParameters) {
+    // Task to receive UART messages
+    char receivedChar;
+    char buffer[MAX_STRING_SIZE];
+    int bufferIndex = 0;
+
+    while (1) {
+        if (uart_is_readable(UART_ID)) {
+            receivedChar = uart_getc(UART_ID);
+            // Check for newline character to indicate end of string
+            if (receivedChar == '\n') {
+                // Null-terminate the string
+                buffer[bufferIndex] = '\0';
+                // Process the received string
+                printf("Received string: %s\n", buffer);
+                // Reset buffer index for the next string
+                bufferIndex = 0;
+            } else if (bufferIndex < MAX_STRING_SIZE - 1) {
+                // Add character to buffer if there's space
+                buffer[bufferIndex++] = receivedChar;
+            } else {
+                // Buffer is full, handle overflow or reset buffer
+                bufferIndex = 0; // Reset buffer index
+            }
+        }
     }
-
-    cyw43_arch_enable_ap_mode("mysterious pico", "password", CYW43_AUTH_WPA2_AES_PSK);
-    ip4_addr_t gw, mask;
-    IP4_ADDR(&gw, 192, 168, 4, 1);
-    IP4_ADDR(&mask, 255, 255, 255, 0);
-
-    dhcp_server_t dhcp_server;
-    dhcp_server_init(&dhcp_server, &gw, &mask);
-
-    mg_mgr_init(&mgr);
-    web_init(&mgr);
-
-    while(true) {
-        mg_mgr_poll(&mgr, 10);
-    }
-
-    cyw43_arch_deinit();
-}
-
-void blink_task(__unused void *params) {
-    blink();
 }
 
 void vLaunch( void) {
     TaskHandle_t task;
-    xTaskCreate(main_task, "TestMainThread", TEST_TASK_STACK_SIZE, NULL, TEST_TASK_PRIORITY, &task);
-    xTaskCreate(blink_task, "BlinkTask", TEST_TASK_STACK_SIZE, NULL, TEST_TASK_PRIORITY, &task);
+    xTaskCreate(uart_receive_task, "uart_receive_task", 2048, NULL, TEST_TASK_PRIORITY, &task);
     vTaskStartScheduler();
 }
 
 int main( void )
-{
+{  
     stdio_init_all();
-    vLaunch();
+    uart_init(UART_ID, BAUD_RATE);
+    gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
+    gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
 
-    return 0;
+    vLaunch();
 }
