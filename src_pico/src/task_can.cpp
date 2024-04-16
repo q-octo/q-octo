9e40b3cf0ae6e85e3bc9f86bc2ec69bc4341f3b2
Britannio <britanniojarrett@gmail.com>
2024-04-16 03:12:58 +0100

    update onReceiveCanPacket control flow

--- src_pico/src/task_can.cpp
+++ src_pico/src/task_can.cpp
@@ -6,6 +6,8 @@
 #include "FreeRTOS.h"
 #include "queue.h"
 
+bool isPowerMonitorPacket(uint32_t packetId);
+
 void taskCAN(void *pvParameters)
 {
   (void)pvParameters; //  To avoid warnings
@@ -34,47 +36,81 @@ void onReceiveCanPacket(int packetLength, uint32_t packetId, uint8_t *packetData
   }
 
   TaskMessage::Message message = {
-      .as = {.canMessage = {.id = packetId, .data = packetData, .len = packetLength}}};
+      .as = {.canMessage =
+                 {.id = packetId, .data = packetData, .len = packetLength}}};
 
-  const uint8_t asMotorId = (packetId & 0xFF00) >> 8;
-  const uint8_t asMotorMasterCanID = (packetId & 0xFF);
-  const bool matchesLeftMotor = asMotorId == CYBERGEAR_CAN_ID_L;
-  const bool matchesRightMotor = asMotorId == CYBERGEAR_CAN_ID_R;
-  
-  // https://dronecan.github.io/Specification/4.1_CAN_bus_transport_layer/#can-frame-format
-  const uint8_t asDroneCanSourceId = (packetId & 0x7F);
+  const uint8_t motorCommType = packetId >> 24;
+  // TODO verify that MASTER_CAN_ID is correct before enforcing this check.
+  const uint8_t motorHostId = packetId & 0xFF;
+  bool isMotorPacket = motorCommType == 0x02 || motorCommType == 0x15;
+  const bool isPowerMonitor = isPowerMonitorPacket(packetId);
 
-  // TODO log the packetId for motor messages.
-  // What are the least significant 8 bits? (0-7)
-  // 8-15 bits are the motor ID
-  // What are 16-28?
-  // They could all be zeros. The motor library ignores the id and just assumes
-  // that the payload consists of position, speed, torque, and temperature.
-  
-  // If they are all zeros, it will be easy to distinguish between a motor
-  // message and a power monitor message.
-  if (matchesLeftMotor)
-  {
-    message.type = TaskMessage::Type::CAN_MESSAGE_MOTOR_L;
-    xQueueSend(dataManagerQueue, &message, 0);
-  }
-
-  if (matchesRightMotor)
-  {
-    message.type = TaskMessage::Type::CAN_MESSAGE_MOTOR_R;
-    xQueueSend(dataManagerQueue, &message, 0);
-  }
-
-  if (!matchesLeftMotor && !matchesRightMotor)
+  if (isPowerMonitor)
   {
     message.type = TaskMessage::Type::CAN_MESSAGE_POWER_MONITOR;
     xQueueSend(dataManagerQueue, &message, 0);
+    return;
+  }
+
+  if (isMotorPacket)
+  {
+    switch (motorCommType)
+    {
+    case 0x02:
+    {
+      const uint8_t motorId = (packetId & 0xFF00) >> 8;
+
+      if (motorId == CYBERGEAR_CAN_ID_L)
+      {
+        message.type = TaskMessage::Type::CAN_MESSAGE_MOTOR_L;
+        xQueueSend(dataManagerQueue, &message, 0);
+      }
+      if (motorId == CYBERGEAR_CAN_ID_R)
+      {
+        message.type = TaskMessage::Type::CAN_MESSAGE_MOTOR_R;
+        xQueueSend(dataManagerQueue, &message, 0);
+      }
+      break;
+    }
+    case 0x15:
+    {
+      // Just send to both motors for now
+      message.type = TaskMessage::Type::CAN_MESSAGE_MOTOR_L;
+      xQueueSend(dataManagerQueue, &message, 0);
+      message.type = TaskMessage::Type::CAN_MESSAGE_MOTOR_R;
+      xQueueSend(dataManagerQueue, &message, 0);
+      break;
+    }
+    default:
+    {
+      isMotorPacket = false;
+      break;
+    }
+    }
+  }
+
+  if (!isMotorPacket && !isPowerMonitor)
+  {
+    // https://dronecan.github.io/Specification/4.1_CAN_bus_transport_layer/#can-frame-format
+    const uint8_t asDroneCanSourceId = (packetId & 0x7F);
+    const uint16_t asDroneCanMessageTypeId = (packetId & 0xFFFF00) >> 8;
     Serial.print("Received packet from unknown device");
     Serial.print(" with id 0x");
     Serial.print(packetId, HEX);
     Serial.println();
-    Serial.print("DroneCAN source id: 0x");
+    Serial.print("DroneCAN source id: 0x"); // hypothetical
     Serial.print(asDroneCanSourceId, HEX);
     Serial.println();
+    Serial.print("DroneCAN message type id: 0x"); // hypothetical
+    Serial.print(asDroneCanMessageTypeId, HEX);
+    Serial.println();
   }
+}
+
+bool isPowerMonitorPacket(uint32_t packetId)
+{
+  // Assuming that we only expect the message frame type.
+  const uint16_t messageTypeId = (packetId & 0xFFFF00) >> 8;
+  const uint16_t batteryInfoDataTypeId = 1092;
+  return messageTypeId == batteryInfoDataTypeId;
 }
