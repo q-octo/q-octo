3ba807705ed946a2298aa418baf99e19dc39ffdd
Niall Morrison <champion.pillip@gmail.com>
2024-04-18 10:41:50 +0100

    Removed companion

--- src_companion/src/microdot/helpers.py
+++
@@ -1,8 +0,0 @@
-try:
-    from functools import wraps
-except ImportError:  # pragma: no cover
-    # MicroPython does not currently implement functools.wraps
-    def wraps(wrapped):
-        def _(wrapper):
-            return wrapper
-        return _
