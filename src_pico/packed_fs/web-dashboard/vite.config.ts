2c1f650d3e947496415299df8e74ea397b5c56ac
Niall <champion.pillip@gmail.com>
2024-04-19 15:07:05 +0100

    Vite bundle to single html

--- src_pico/packed_fs/web-dashboard/vite.config.ts
+++ src_pico/packed_fs/web-dashboard/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
+import { viteSingleFile } from 'vite-plugin-singlefile';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [preact()],
+	plugins: [preact(), viteSingleFile()],
 	build: {
 		outDir: '../files'
 	}
