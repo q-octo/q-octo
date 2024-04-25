acb508bda2393421a9f5cd0dfe08fc17a3fb0db1
Britannio <britanniojarrett@gmail.com>
2024-04-25 20:03:41 +0100

    delete src_pico/packed_fs (moved to src_companion)

--- src_pico/packed_fs/web-dashboard/src/util/WebSocket.ts
+++
@@ -1,9 +0,0 @@
-const ws = new WebSocket('ws://rover.local:81/');
-//const ws = new WebSocket('ws://localhost:8080');
-
-// On websocket open
-ws.onopen = () => {
-    console.log('Connected to server');
-};
-
-export default ws;
