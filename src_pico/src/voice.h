bff970d562533b90d32b8b1ffca703790b01ad54
Britannio <britanniojarrett@gmail.com>
2024-05-03 21:16:41 +0100

    add voice class

---
+++ src_pico/src/voice.h
@@ -0,0 +1,22 @@
+#pragma once
+
+class Voice {
+public:
+    typedef enum {
+        OK,
+        LOW_BATTERY,
+        TX_LOST,
+        TX_RESTORED,
+        MOTOR_ERROR,
+        DISARMED,
+        POWER_ON,
+        NAVIGATE_VIA_ONBOARD_COMPUTER,
+        NAVIGATE_VIA_FLIGHT_CONTROLLER,
+        NAVIGATE_VIA_RADIO,
+        DASHBOARD_ENABLED,
+        DASHBOARD_DISABLED,
+        FOLDING_WHEELS,
+        STAND_CLEAR,
+    } Announcement;
+private:
+};
