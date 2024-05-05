caa5a9b1c881fc8982c54a6bc272372adeed1a3e
Britannio <britanniojarrett@gmail.com>
2024-05-05 13:07:54 +0100

    add more checks to EEPROm

--- src_pico/src/storage.cpp
+++ src_pico/src/storage.cpp
@@ -3,7 +3,12 @@
 
 void Storage::init()
 {
-    EEPROM.begin(sizeof(State));
+    const size_t size = sizeof(State);
+    if (size > 4096) {
+        Serial.println("[ERROR] State struct cannot fit in EEPROM");
+        return;
+    }
+    EEPROM.begin(size);
     // TODO remove this workaround as it makes this system redundant
     // We shuold figure out how to check if this is the first read or not
     save();
@@ -13,11 +18,17 @@ void Storage::init()
 void Storage::save()
 {
     EEPROM.put(0, state);
-    EEPROM.commit();
-    // Seeing this too often? The EEPROM has a limited number of writes so an
-    // effort should be made to reduce the number of writes.
-    Serial.println("Saved state to EEPROM");
-    notifyStateUpdate();
+    if (EEPROM.commit())
+    {
+        // Seeing this too often? The EEPROM has a limited number of writes so an
+        // effort should be made to reduce the number of writes.
+        Serial.println("Saved state to EEPROM");
+        notifyStateUpdate();
+    }
+    else
+    {
+        Serial.println("Failed to save state to EEPROM");
+    }
 }
 
 void Storage::notifyStateUpdate()
