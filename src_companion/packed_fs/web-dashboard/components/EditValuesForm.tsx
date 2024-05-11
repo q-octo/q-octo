0d7f5f509d623ff272f23b0f082176936f6f67c7
Niall <champion.pillip@gmail.com>
2024-05-11 17:52:45 +0100

    Added all fields

--- src_companion/packed_fs/web-dashboard/components/EditValuesForm.tsx
+++ src_companion/packed_fs/web-dashboard/components/EditValuesForm.tsx
@@ -4,21 +4,116 @@ import ws from "../src/util/WebSocket";
 import { UpdateUnion } from "../src/generated/fbs";
 import { RoverState } from "../src/types";
 
-enum Field {
-    Bats = "bats",
-    LowV = "lowV",
-    CritV = "critV",
-    TorqueLimit = "torqueLimit",
-    SpeedLimit = "speedLimit",
-    CurrentLimit = "currentLimit",
-    Kp = "Kp",
-    Ki = "Ki",
-    RSSIThres = "RSSIThres",
-    LinkQualityThres = "LinkQualityThres",
-    ShutdownVoltage = "ShutdownVoltage",
-    CriticalVoltage = "CriticalVoltage",
-    RadioTimeout = "RadioTimeout",
-    WebServerOnLaunch = "WebServerOnLaunch",
+
+type FieldParams = {
+    field_id: number,
+    label: string,
+    onChange: (e: any) => void,
+    handleSubmit: (fieldName: number, event: any) => void
+    formValues: object,
+    rover: RoverState
+    max?: number,
+    min?: number,
+    step?: number
+}
+// A generic form field component
+const FormField = ({ field_id, label, handleSubmit, onChange, formValues, rover, max, min, step }: FieldParams) => {
+
+    // Decides what colour to render in the background of the input fields
+    const formColour = (field_id: number) => {
+
+        const colorMatch = "bg-green-100";
+        const colorNoMatch = "bg-red-100";
+
+        switch (field_id) {
+            case UpdateUnion.UpdateBatteries:
+                if (formValues[UpdateUnion.UpdateBatteries] == rover.batteries) return colorMatch;
+                break;
+            case UpdateUnion.UpdateLowVoltageThreshold:
+                if (formValues[UpdateUnion.UpdateLowVoltageThreshold] == rover.low_voltage_threshold) return colorMatch;
+                break;
+            case UpdateUnion.UpdateCriticalVoltageThreshold:
+                if (formValues[UpdateUnion.UpdateCriticalVoltageThreshold] == rover.critical_voltage_threshold) return colorMatch;
+                break;
+            case UpdateUnion.UpdateMaxTorque:
+                if ((formValues[UpdateUnion.UpdateMaxTorque] == rover.motors.motor1.torque_limit) && (formValues[UpdateUnion.UpdateMaxTorque] == rover.motors.motor2.torque_limit)) {
+                    return colorMatch;
+                }
+                break;
+            case UpdateUnion.UpdateMaxSpeed:
+                if ((formValues[UpdateUnion.UpdateMaxSpeed] == rover.motors.motor1.speed_limit) && (formValues[UpdateUnion.UpdateMaxSpeed] == rover.motors.motor2.speed_limit)) {
+                    return colorMatch;
+                }
+                break;
+            case UpdateUnion.UpdateMaxCurrent:
+                if ((formValues[UpdateUnion.UpdateMaxCurrent] == rover.motors.motor1.current_limit) && (formValues[UpdateUnion.UpdateMaxCurrent] == rover.motors.motor2.current_limit)) {
+                    return colorMatch;
+                }
+                break;
+            case UpdateUnion.UpdateMotorKi:
+                if ((formValues[UpdateUnion.UpdateMotorKi] == rover.motors.motor1.Ki) && (formValues[UpdateUnion.UpdateMotorKi] == rover.motors.motor2.Ki)) {
+                    return colorMatch;
+                }
+                break;
+            case UpdateUnion.UpdateMotorKp:
+                if ((formValues[UpdateUnion.UpdateMotorKp] == rover.motors.motor1.Kp) && (formValues[UpdateUnion.UpdateMotorKp] == rover.motors.motor2.Kp)) {
+                    return colorMatch;
+                }
+                break;
+            case UpdateUnion.UpdateLowVoltageThreshold:
+                if (formValues[UpdateUnion.UpdateLowVoltageThreshold] == rover.low_voltage_threshold) return colorMatch;
+                break;
+            case UpdateUnion.UpdateCriticalVoltageThreshold:
+                if (formValues[UpdateUnion.UpdateCriticalVoltageThreshold] == rover.critical_voltage_threshold) return colorMatch;
+                break;
+            case UpdateUnion.UpdateLinkQualityThreshold:
+                if (formValues[UpdateUnion.UpdateLinkQualityThreshold] == rover.link_quality_threshold) return colorMatch;
+                break;
+            case UpdateUnion.UpdateRssiThreshold:
+                if (formValues[UpdateUnion.UpdateRssiThreshold] == rover.rssi_threshold) return colorMatch;
+                break;
+            case UpdateUnion.UpdateStartWebServerOnLaunch:
+                if (formValues[UpdateUnion.UpdateStartWebServerOnLaunch] == rover.web_server_on_launch) return colorMatch;
+                break;
+            case UpdateUnion.UpdateCrsfLinkStatsTimeout:
+                if (formValues[UpdateUnion.UpdateCrsfLinkStatsTimeout] == rover.radio_timeout) return colorMatch;
+                break;
+
+        }
+
+        return colorNoMatch;
+    }
+
+    // Parameters for the input field (step, min, max)
+    if (step == undefined) step = 0.1;
+
+    let params;
+
+    if (min == undefined || max == undefined) {
+        params = { step: step };
+    }
+    else {
+        params = { step: step, min: min, max: max };
+    }
+
+    return (
+        <form onSubmit={(e) => handleSubmit(field_id, e)} className="mb-4">
+            <label htmlFor={`${field_id}`} className="block text-sm font-medium text-gray-700">
+                {label}
+            </label>
+            <div className="mt-1 flex rounded-md shadow-sm">
+                <input type="number" id={`${field_id}`} name={`${field_id}`} className={`${formColour(field_id)} focus:ring-blue-500 focus:border-blue-500 block w-full pl-2 sm:text-sm border-gray-300 rounded-l-md`}
+                    required
+                    onChange={onChange}
+                    {...params}
+                    key={field_id}
+                />
+                <button type="submit" className="ml-2 bg-blue-500 text-white py-2 px-4 rounded-r-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
+                    Save
+                </button>
+            </div>
+        </form>
+    );
 }
 
 /**
@@ -26,173 +121,79 @@ enum Field {
  */
 export const EditValuesForm = ({ rover }: { rover: RoverState }) => {
 
-    type FieldParams = {
-        field_id: string,
-        label: string,
-        max?: number,
-        min?: number,
-        step?: number
-    }
+    // Default values for the form fields (initial state)
+    const FieldDefaults = [
+        { key: UpdateUnion.UpdateBatteries, value: rover.batteries },
+        { key: UpdateUnion.UpdateLowVoltageThreshold, value: rover.low_voltage_threshold },
+        { key: UpdateUnion.UpdateCriticalVoltageThreshold, value: rover.critical_voltage_threshold },
+        { key: UpdateUnion.UpdateMaxTorque, value: rover.motors.motor1.torque_limit },
+        { key: UpdateUnion.UpdateMaxSpeed, value: rover.motors.motor1.speed_limit },
+        { key: UpdateUnion.UpdateMaxCurrent, value: rover.motors.motor1.current_limit },
+        { key: UpdateUnion.UpdateMotorKi, value: rover.motors.motor1.Kp },
+        { key: UpdateUnion.UpdateMotorKp, value: rover.motors.motor1.Ki },
+        { key: UpdateUnion.UpdateLowVoltageThreshold, value: rover.low_voltage_threshold },
+        { key: UpdateUnion.UpdateCriticalVoltageThreshold, value: rover.critical_voltage_threshold },
+        { key: UpdateUnion.UpdateLinkQualityThreshold, value: rover.link_quality_threshold },
+        { key: UpdateUnion.UpdateRssiThreshold, value: rover.rssi_threshold },
+        { key: UpdateUnion.UpdateStartWebServerOnLaunch, value: rover.web_server_on_launch },
+        { key: UpdateUnion.UpdateCrsfLinkStatsTimeout, value: rover.radio_timeout },
+    ]
 
-    // A single form field
-    const FormField = ({field_id,label,max,min,step} :  FieldParams) => {
+    let key_value = {}
+    FieldDefaults.forEach((field) => {
+        key_value[field.key] = field.value;
+    })
 
 
-        // Parameters for the input field (step, min, max)
-        if (step == undefined) step = 0.1;
-        
-        let params;
-
-        if (min == undefined || max == undefined) {
-            params = {step: step};
-        }
-        else {
-            params = {step: step, min: min, max: max};
-        }
-
-        return (
-            <form onSubmit={(e) => handleSubmit(field_id, e)} className="mb-4">
-                <label htmlFor={`${field_id}`} className="block text-sm font-medium text-gray-700">
-                    {label}
-                </label>
-                <div className="mt-1 flex rounded-md shadow-sm">
-                    <input type="number" id={`${field_id}`} name={`${field_id}`} className={`${formColour(field_id)} focus:ring-blue-500 focus:border-blue-500 block w-full pl-2 sm:text-sm border-gray-300 rounded-l-md`} 
-                        required
-                        onChange={handleChange}
-                        {...params}
-                    />
-                    <button type="submit" className="ml-2 bg-blue-500 text-white py-2 px-4 rounded-r-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
-                        Save
-                    </button>
-                </div>
-            </form>
-        );
-    }
-    
     // Holds state for indicating the colour of the input fields
-    const [formValues, setFormValues] = useState({
-        1: 0,
-        2: 0,
-        3: 0,
-        4: 0
-    });
+    const [formValues, setFormValues] = useState(key_value);
 
     // Function to handle form submission for each field
-	const handleSubmit = (fieldName, event) => {
+    const handleSubmit = (fieldName, event) => {
         event.preventDefault();
 
-		// Loop through the form data to get the values
-		const formData = new FormData(event.target);
-		const data = {};
-		for (let [key, value] of formData.entries()) {
-			data[key] = parseFloat(value);
-			console.log(key, value);
-		}
+        // Loop through the form data to get the values
+        const formData = new FormData(event.target);
+        const data = {};
+        for (let [key, value] of formData.entries()) {
+            data[key] = parseFloat(value);
+            console.log(key, value);
+        }
 
-		// Send the update
-		ws.send(SendUpdate(fieldName, data[fieldName]));
-	};
-
-
-	const handleChange = (e) => {
-		const { name, value } = e.target;
-		setFormValues({ ...formValues, [name]: value });
-	}
-
-	// Decides what colour to render in the background of the input fields
-    // TODO: Update with new field names
-	const formColour = (field_id: number) => {
+        // Send the update
+        ws.send(SendUpdate(fieldName, data[fieldName]));
+    };
 
 
-        const colorMatch = "bg-green-100";
-        const colorNoMatch = "bg-red-100";
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormValues({ ...formValues, [name]: value });
+    }
 
-		console.log(formValues);
-		console.log(rover);
+    return (
+        <div className="bg-slate-200 p-4 sm:p-6 max-w-lg mx-auto rounded-lg shadow-md">
+            <h2 className="text-xl sm:text-2xl font-bold mb-4">Edit Values</h2>
+            <h3 className="text-lg sm:text-xl font-bold mb-4">❗ Motor Limits</h3>
+            <FormField field_id={UpdateUnion.UpdateMaxTorque} label="Torque Limit"  onChange={handleChange} handleSubmit={handleSubmit} formValues={formValues} rover={rover} />
+            <FormField field_id={UpdateUnion.UpdateMaxSpeed} label="Speed Limit" onChange={handleChange} handleSubmit={handleSubmit} formValues={formValues} rover={rover} />
+            <FormField field_id={UpdateUnion.UpdateMaxCurrent} label="Current Limit" onChange={handleChange} handleSubmit={handleSubmit} formValues={formValues} rover={rover} />
 
-		switch (field_id) {
-			case UpdateUnion.UpdateBatteries:
-				if (formValues[1] == rover.batteries) return colorMatch;
-				break;
-			case UpdateUnion.UpdateLowVoltageThreshold:
-				if (formValues[2] == rover.low_voltage_threshold) return colorMatch;
-				break;
-			case UpdateUnion.UpdateCriticalVoltageThreshold:
-				if (formValues[3] == rover.critical_voltage_threshold) return colorMatch;
-				break;
-			case UpdateUnion.UpdateMaxTorque:
-				if ((formValues[4] == rover.motors.motor1.torque_limit) && (formValues[4] == rover.motors.motor2.torque_limit)) {
-					return colorMatch;
-				}
-		}
+            <h3 className="text-lg sm:text-xl font-bold mb-4">⚡ Voltage Thresholds</h3>
+            <FormField field_id={UpdateUnion.UpdateLowVoltageThreshold} label="Low Voltage Threshold" onChange={handleChange} handleSubmit={handleSubmit} formValues={formValues} rover={rover} />
+            <FormField field_id={UpdateUnion.UpdateCriticalVoltageThreshold} label="Critical Voltage Threshold" onChange={handleChange} handleSubmit={handleSubmit} formValues={formValues} rover={rover} />
 
-		return colorNoMatch;
-	}
+            <h3 className="text-lg sm:text-xl font-bold mb-4">📻 Radio Settings</h3>
+            <FormField field_id={UpdateUnion.UpdateRssiThreshold} label="RSSI Threshold" onChange={handleChange} handleSubmit={handleSubmit} formValues={formValues} rover={rover} />
+            <FormField field_id={UpdateUnion.UpdateLinkQualityThreshold} label="Link Quality Threshold" onChange={handleChange} handleSubmit={handleSubmit} formValues={formValues} rover={rover} />
+            <FormField field_id={UpdateUnion.UpdateCrsfLinkStatsTimeout} label="CRSF Link Stats Timeout" onChange={handleChange} handleSubmit={handleSubmit} formValues={formValues} rover={rover} />
 
-	return (
-		<div className="bg-slate-200 p-4 sm:p-6 max-w-lg mx-auto rounded-lg shadow-md">
-			<h2 className="text-xl sm:text-2xl font-bold mb-4">Edit Values</h2>
+            <h3 className="text-lg sm:text-xl font-bold mb-4">🔧 Motor PID Constants</h3>
+            <FormField field_id={UpdateUnion.UpdateMotorKi} label="Motor Ki" onChange={handleChange} handleSubmit={handleSubmit} formValues={formValues} rover={rover} />
+            <FormField field_id={UpdateUnion.UpdateMotorKp} label="Motor Kp" onChange={handleChange} handleSubmit={handleSubmit} formValues={formValues} rover={rover} />
 
-			{/* Number of batteries */}
-			<form onSubmit={(e) => handleSubmit(UpdateUnion.UpdateBatteries, e)} className="mb-4">
-				<label htmlFor={`${UpdateUnion.UpdateBatteries}`} className="block text-sm font-medium text-gray-700">
-					Number of batteries
-				</label>
-				<div className="mt-1 flex rounded-md shadow-sm">
-					<input type="number" id={`${UpdateUnion.UpdateBatteries}`} name={`${UpdateUnion.UpdateBatteries}`} className={`${formColour(Field.Bats)} focus:ring-blue-500 focus:border-blue-500 block w-full pl-2 sm:text-sm border-gray-300 rounded-l-md`} required
-						onChange={handleChange}
-					/>
-					<button type="submit" className="ml-2 bg-blue-500 text-white py-2 px-4 rounded-r-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
-						Save
-					</button>
-				</div>
-			</form>
+            <h3 className="text-lg sm:text-xl font-bold mb-4">⚙ Other Settings</h3>
+            <FormField field_id={UpdateUnion.UpdateBatteries} label="Number of batteries" onChange={handleChange} handleSubmit={handleSubmit} formValues={formValues} rover={rover} />
 
-			{/* Low Voltage Threshold */}
-			<form onSubmit={(e) => handleSubmit(UpdateUnion.UpdateLowVoltageThreshold, e)} className="mb-4">
-				<label htmlFor={`${UpdateUnion.UpdateLowVoltageThreshold}`} className="block text-sm font-medium text-gray-700">
-					Low Voltage threshold
-				</label>
-				<div className="mt-1 flex rounded-md shadow-sm">
-					<input type="number" step="0.1" id={`${UpdateUnion.UpdateLowVoltageThreshold}`} name={`${UpdateUnion.UpdateLowVoltageThreshold}`} className={`${formColour(Field.LowV)} focus:ring-blue-500 focus:border-blue-500 block w-full pl-2 sm:text-sm border-gray-300 rounded-l-md`} required
-						onChange={handleChange}
-					/>
-					<button type="submit" className="ml-2 bg-blue-500 text-white py-2 px-4 rounded-r-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
-						Save
-					</button>
-				</div>
-			</form>
-
-			{/* Critical Voltage Threshold */}
-			<form onSubmit={(e) => handleSubmit(UpdateUnion.UpdateCriticalVoltageThreshold, e)} className="mb-4">
-				<label htmlFor={`${UpdateUnion.UpdateCriticalVoltageThreshold}`} className="block text-sm font-medium text-gray-700">
-					Critical Voltage Threshold
-				</label>
-				<div className="mt-1 flex rounded-md shadow-sm">
-					<input type="number" step="0.1" id={`${UpdateUnion.UpdateCriticalVoltageThreshold}`} name={`${UpdateUnion.UpdateCriticalVoltageThreshold}`} className={`${formColour(Field.CritV)} focus:ring-blue-500 focus:border-blue-500 block w-full pl-2 sm:text-sm border-gray-300 rounded-l-md`} required
-						onChange={handleChange}
-					/>
-					<button type="submit" className="ml-2 bg-blue-500 text-white py-2 px-4 rounded-r-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
-						Save
-					</button>
-				</div>
-			</form>
-
-
-			{/* Torque Limit */}
-			<form onSubmit={(e) => handleSubmit(UpdateUnion.UpdateMaxTorque, e)} className="mb-4">
-				<label htmlFor="torqueLimit" className="block text-sm font-medium text-gray-700">
-					Torque Limit Left
-				</label>
-				<div className="mt-1 flex rounded-md shadow-sm">
-					<input type="number" step="0.1" id={`${UpdateUnion.UpdateMaxTorque}`} name={`${UpdateUnion.UpdateMaxTorque}`} className={`${formColour(Field.TorqueLimit)} focus:ring-blue-500 focus:border-blue-500 block w-full pl-2 sm:text-sm border-gray-300 rounded-l-md" required`}
-						onChange={handleChange}
-					/>
-					<button type="submit" className="ml-2 bg-blue-500 text-white py-2 px-4 rounded-r-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
-						Save
-					</button>
-				</div>
-			</form>
-		</div>
-	);
+        </div>
+    );
 };
--- src_companion/packed_fs/web-dashboard/src/index.tsx
+++ src_companion/packed_fs/web-dashboard/src/index.tsx
@@ -9,6 +9,7 @@ import { RobotToObj } from './util/RobotToObj';
 import { SendUpdate } from './util/SendUpdate';
 import { Round, Round4 } from './util/Round';
 import ws from './util/WebSocket';
+import { EditValuesForm } from '../components/EditValuesForm';
 
 const Motor = ({motor, name}) => {
 
@@ -100,131 +101,6 @@ const Dashboard = ({ rover }: { rover: RoverState }) => {
 	);
 };
 
-
-/**
- * Component for the input form.
- */
-const EditValuesForm = ({ rover }: { rover: RoverState }) => {
-	// Function to handle form submission for each field
-	const handleSubmit = (fieldName, event) => {
-		event.preventDefault();
-
-		// Loop through the form data to get the values
-		const formData = new FormData(event.target);
-		const data = {};
-		for (let [key, value] of formData.entries()) {
-			data[key] = parseFloat(value);
-			console.log(key, value);
-		}
-
-		// Send the update
-		ws.send(SendUpdate(fieldName, data[fieldName]));
-	};
-
-	const [formValues, setFormValues] = useState({
-		1: 0,
-		2: 0,
-		3: 0,
-		4: 0
-	});
-
-	const handleChange = (e) => {
-		const { name, value } = e.target;
-		setFormValues({ ...formValues, [name]: value });
-	}
-
-	// Decides what colour to render in the background of the input fields
-	const formColour = (fieldname: string) => {
-
-		console.log(formValues);
-		console.log(rover);
-
-		switch (fieldname) {
-			case 'bats':
-				if (formValues[1] == rover.batteries) return "bg-green-100";
-				break;
-			case 'lowV':
-				if (formValues[2] == rover.low_voltage_threshold) return "bg-green-100";
-				break;
-			case 'critV':
-				if (formValues[3] == rover.critical_voltage_threshold) return "bg-green-100";
-				break;
-			case 'torqueLimit':
-				if ((formValues[4] == rover.motors.motor1.torque_limit) && (formValues[4] == rover.motors.motor2.torque_limit)) {
-					return "bg-green-100";
-				}
-		}
-
-		return "bg-red-100";
-	}
-
-	return (
-		<div className="bg-slate-200 p-4 sm:p-6 max-w-lg mx-auto rounded-lg shadow-md">
-			<h2 className="text-xl sm:text-2xl font-bold mb-4">Edit Values</h2>
-
-			{/* Number of batteries */}
-			<form onSubmit={(e) => handleSubmit(UpdateUnion.UpdateBatteries, e)} className="mb-4">
-				<label htmlFor={`${UpdateUnion.UpdateBatteries}`} className="block text-sm font-medium text-gray-700">
-					Number of batteries
-				</label>
-				<div className="mt-1 flex rounded-md shadow-sm">
-					<input type="number" id={`${UpdateUnion.UpdateBatteries}`} name={`${UpdateUnion.UpdateBatteries}`} className={`${formColour('bats')} focus:ring-blue-500 focus:border-blue-500 block w-full pl-2 sm:text-sm border-gray-300 rounded-l-md`} required
-						onChange={handleChange}
-					/>
-					<button type="submit" className="ml-2 bg-blue-500 text-white py-2 px-4 rounded-r-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
-						Save
-					</button>
-				</div>
-			</form>
-
-			{/* Low Voltage Threshold */}
-			<form onSubmit={(e) => handleSubmit(UpdateUnion.UpdateLowVoltageThreshold, e)} className="mb-4">
-				<label htmlFor={`${UpdateUnion.UpdateLowVoltageThreshold}`} className="block text-sm font-medium text-gray-700">
-					Low Voltage threshold
-				</label>
-				<div className="mt-1 flex rounded-md shadow-sm">
-					<input type="number" step="0.1" id={`${UpdateUnion.UpdateLowVoltageThreshold}`} name={`${UpdateUnion.UpdateLowVoltageThreshold}`} className={`${formColour('lowV')} focus:ring-blue-500 focus:border-blue-500 block w-full pl-2 sm:text-sm border-gray-300 rounded-l-md`} required
-						onChange={handleChange}
-					/>
-					<button type="submit" className="ml-2 bg-blue-500 text-white py-2 px-4 rounded-r-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
-						Save
-					</button>
-				</div>
-			</form>
-
-			{/* Critical Voltage Threshold */}
-			<form onSubmit={(e) => handleSubmit(UpdateUnion.UpdateCriticalVoltageThreshold, e)} className="mb-4">
-				<label htmlFor={`${UpdateUnion.UpdateCriticalVoltageThreshold}`} className="block text-sm font-medium text-gray-700">
-					Critical Voltage Threshold
-				</label>
-				<div className="mt-1 flex rounded-md shadow-sm">
-					<input type="number" step="0.1" id={`${UpdateUnion.UpdateCriticalVoltageThreshold}`} name={`${UpdateUnion.UpdateCriticalVoltageThreshold}`} className={`${formColour('critV')} focus:ring-blue-500 focus:border-blue-500 block w-full pl-2 sm:text-sm border-gray-300 rounded-l-md`} required
-						onChange={handleChange}
-					/>
-					<button type="submit" className="ml-2 bg-blue-500 text-white py-2 px-4 rounded-r-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
-						Save
-					</button>
-				</div>
-			</form>
-
-
-			{/* Torque Limit */}
-			<form onSubmit={(e) => handleSubmit(UpdateUnion.UpdateMaxTorque, e)} className="mb-4">
-				<label htmlFor="torqueLimit" className="block text-sm font-medium text-gray-700">
-					Torque Limit Left
-				</label>
-				<div className="mt-1 flex rounded-md shadow-sm">
-					<input type="number" step="0.1" id={`${UpdateUnion.UpdateMaxTorque}`} name={`${UpdateUnion.UpdateMaxTorque}`} className={`${formColour('torqueLimitLeft')} focus:ring-blue-500 focus:border-blue-500 block w-full pl-2 sm:text-sm border-gray-300 rounded-l-md" required`}
-						onChange={handleChange}
-					/>
-					<button type="submit" className="ml-2 bg-blue-500 text-white py-2 px-4 rounded-r-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
-						Save
-					</button>
-				</div>
-			</form>
-		</div>
-	);
-};
 
 /**
  * Root component of the application.
