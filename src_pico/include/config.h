afdf5d2f1376f9f0f57680fe13081426c22d17ef
Britannio <britanniojarrett@gmail.com>
2024-05-12 15:20:26 +0100

    disable storage wiping

--- src_pico/include/config.h
+++ src_pico/include/config.h
@@ -10,7 +10,7 @@ static const int CFG_ENABLE_ONBOARD_COMPUTER = 0;
 static const int CFG_WAIT_FOR_USB_SERIAL = 0;
 
 static const int CFG_DEBUG_LIST_TASKS = 0;
-static const int CFG_DEBUG_WIPE_STORAGE = 1;
+static const int CFG_DEBUG_WIPE_STORAGE = 0;
 
 // Pins
 static const int CFG_RC_UART_TX = 0;
@@ -19,5 +19,7 @@ static const int CFG_COMPANION_UART_TX = 2;
 static const int CFG_COMPANION_UART_RX = 3;
 static const int CFG_ONBOARD_COMPUTER_UART_TX = 4;
 static const int CFG_ONBOARD_COMPUTER_UART_RX = 5;
+static const int CFG_VOICE_UART_TX = 6;
+static const int CFG_VOICE_UART_RX = 7;
 static const int CFG_BTN_START_WEB_SERVER = 14;
 static const int CFG_BTN_DISABLE_MOTORS = 15;
