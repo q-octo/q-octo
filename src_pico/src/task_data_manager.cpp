2d90302a799a7a3c8eaa3070d6846dd8f4e321d3
Britannio <britanniojarrett@gmail.com>
2024-05-14 13:32:27 +0100

    remov temporary battery data code

--- src_pico/src/task_data_manager.cpp
+++ src_pico/src/task_data_manager.cpp
@@ -51,16 +51,7 @@ void DataManager::receiveMessage(const DataManager::Message &message) {
   switch (message.type) {
     case STATE_MOTORS:
       state.motors = message.as.motors;
-      broadcastStateUpdate();
-      // TODO remove this temporary code used to test the transmitter
-      rcMessage.type = TaskRC::MessageType::BATTERY;
-      rcMessage.as.battery = {
-        .voltage = 12.0f,
-        .current = 13.0f,
-        .fuel = 14,
-        .percent = 15
-      };
-      TaskRC::receiveMessage(rcMessage);
+      broadcastStateUpdate(); 
       break;
     case STATE_RC:
       state.rc = message.as.rc;
